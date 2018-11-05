9c4da19180f65dc8f48f34ce6ef1e29998e5783e
jayavelusaravanan11 <44713317+jayavelusaravanan11@users.noreply.github.com>
2018-11-05 02:44:27 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
-#python ./src/h1b_counting.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+#The python program written by me accepts five arguments 
+#1. The name of the program viz h1b_analysis.py
+#2. The name of the input file 
+#3. The column name of case_status as column names change between inputfiles . Note the awkward case . The program converts to uppsercase
+#4. The column name of soc_name as column names change between inputfiles . Note the awkward case . The program converts to uppsercase
+#5. The column name of employer_statel as column names change between inputfiles . Note the awkward case . The program converts to uppsercase
+#6 Two outputfiles provided as individual arguments
+python ./src/h1b_analysis.py ./input/H1B_FY_2015-split1.csv case_statuS soc_NamE EMPLOyER_state ./output/top_10_occupations.txt ./output/top_10_states.txt
