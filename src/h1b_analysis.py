cb0fbbb17c5b8d8e651d6b15429efbaca5fa9b85
jayavelusaravanan11 <44713317+jayavelusaravanan11@users.noreply.github.com>
2018-11-05 04:51:52 -0500

    Update h1b_analysis.py

--- src/h1b_analysis.py
+++ src/h1b_analysis.py
@@ -105,7 +105,7 @@ def main():
 			perc_list_occupations.append(per_value_round)
 		#Join three lists , sort them in reverse order and get the top 10 values for occupations
 		pair_occupations=[list(a) for a in zip(cert_occupations_l,freq_list_occupations,perc_list_occupations)]
-		list_top_10_occupations=sorted(pair_occupations,key=lambda pair_occupations: pair_occupations[1], reverse=True)[:5]
+		list_top_10_occupations=sorted(pair_occupations,key=lambda pair_occupations: pair_occupations[1], reverse=True)[:9]
 		#Call function get_unique_values to get the list of unique states with certifications
 		cert_states_l=get_unique_values(cert_states)
 		#count(*) unique certified states and insert into list
